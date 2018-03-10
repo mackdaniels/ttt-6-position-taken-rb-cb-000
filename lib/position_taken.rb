0c1e32bba46e0ec4206579b2aedb60d702778e95
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-10 20:47:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,9 @@
 def position_taken?(board, index)
   if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
   end
 end
